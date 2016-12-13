154f38cf581efa10cb112e7298fa3edd034cabe1
Christian Carey <christianrcarey@gmail.com>
2016-12-13 10:53:00 -0500

    Get in_rows_of method working

--- app/presenters/collection_presenter.rb
+++ app/presenters/collection_presenter.rb
@@ -2,19 +2,21 @@ class CollectionPresenter < BasePresenter
 
   def in_rows_of(num_columns, options = {})
     # TODO handle incorrect input
-    # TODO console says rendering but no outpu in the window
+    # TODO console says rendering but no output in the window
     breakpoint  = options[:breakpoint] || "md"
     partial     = options[:partial]
     column_size = 12 / num_columns
+    result = ""
     collection.each_slice(num_columns) do |slice|
-      content_tag :div, class: 'row' do 
+      result += "<div class='row'>"
         slice.each do |element|
-          content_tag :div, class: "col-#{breakpoint}-#{column_size}" do 
-            render partial: partial || element
-          end
+          result += "<div class='col-#{breakpoint}-#{column_size}'>"          
+            result += render partial: partial || element
+          result += "</div>"
         end
-      end
+      result += "</div>"
     end
+    result.html_safe
   end
 
   private
